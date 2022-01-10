ba41f21fd666a81bddf97495e18d78685108e969
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-01-10 14:04:49 +0700

    Handled login and logout task

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,21 +1,40 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { fork, take } from 'redux-saga/effects';
+import { call, fork, put, take } from 'redux-saga/effects';
 import { authActions, LoginPayLoad } from './authSlice';
 
 function* handleLogin(payload: LoginPayLoad) {
-  console.log('Handle login', payload);
+  try {
+    // yield call(loginApi, data)
+    localStorage.setItem('access_token', 'adasd');
+    yield put(
+      authActions.loginSuccess({
+        id: 1,
+        name: 'Thong',
+      })
+    );
+    //redirect to admin page
+  } catch (error: any) {
+    yield put(authActions.loginFail(error.massage));
+  }
 }
 
 function* handleLogout() {
   console.log('Handle logout');
+  localStorage.removeItem('access_token');
+  yield put(authActions.logout());
+  //redirect to login page
 }
 
 function* watchLoginFlow() {
   while (true) {
-    const action: PayloadAction<LoginPayLoad> = yield take(authActions.login.type); //blocking
-    yield fork(handleLogin, action.payload);
-    yield take(authActions.logout.type); //blocking
-    yield fork(handleLogout);
+    const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+    if (!isLoggedIn) {
+      const action: PayloadAction<LoginPayLoad> = yield take(authActions.login.type); // blocking
+      yield fork(handleLogin, action.payload);
+    }
+
+    yield take(authActions.logout.type); // blocking
+    yield call(handleLogout); // blocking - wait for completing handle logout
   }
 }
 
